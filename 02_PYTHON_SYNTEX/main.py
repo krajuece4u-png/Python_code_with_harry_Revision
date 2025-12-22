44cf3bab21c66e2c71f3ef5c95e0e405887be3f1
Kaj Raju <kraju.ece4u@gmail.com>
2025-12-22 23:49:22 +0530

    Variable and user input

---
+++ 02_PYTHON_SYNTEX/main.py
@@ -0,0 +1,10 @@
+print("Hello, Raju")
+print("I am learning Python.")
+print("How are you?")
+print(3)
+
+# Worong Indentation
+# if 5 > 2:
+#     print("Five is greater than two!")
+# print("Hello, World!")
+#     print("Hello, World!")
